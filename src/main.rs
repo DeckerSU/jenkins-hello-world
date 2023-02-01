e3ce595819e0c2193958b01499c9db68af643e06
DeckerSU <deckersu@protonmail.com>
2023-02-01 19:57:17 +0100

    add unit tests
    also it's a commit signing test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// https://doc.rust-lang.org/rust-by-example/testing/unit_testing.html
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        let flag = true;
+        assert_eq!(flag, true);
+    }
+}
